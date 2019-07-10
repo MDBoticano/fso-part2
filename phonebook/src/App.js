0239ced88495f35d4d6fc00b89230b585fcd4ca2
MDBoticano <MDBoticano@users.noreply.github.com>
2019-07-10 16:19:35 -0700

    Exercise 2.8 The Phonebook Step3 (phone # inputs)

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -5,37 +5,42 @@ const App = () => {
     { name: 'Arto Hellas' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
 
   const listPersons = () => persons.map(person => 
-    <p key={person.name}>{person.name}</p>
+    <p key={person.name}>{person.name} {person.number}</p>
   )
   
   const handleSubmit = (event) => {
     event.preventDefault();
     // console.log()
 
-    // Don't allow addition of already existing names: if/else
-    // if (persons.find( (person) => person.name === newName) !== undefined) {
-    //   alert(`${newName} is already added to the phonebook`);
-    // } else {
-    //   const personObject = {
-    //     name: newName
-    //   }
-    //   setPersons(persons.concat(personObject));
-    // }    
+    // Don't allow addition of already existing names
+    let nameExists = persons.find(person => person.name === newName);
+    let numberExists = persons.find(person => person.number === newNumber);
 
-    // Don't allow addition of already existing names: ternary
-    persons.find(person => person.name === newName) !== undefined
-      ? alert(`${newName} is already added to the phonebook`)
-      : setPersons(persons.concat({name: newName}))
-
-    setNewName('');
+    if (nameExists !== undefined) {
+      alert(`${newName} is already added to the phonebook`);
+      setNewName('');
+    } else if (numberExists !== undefined) {
+      alert(`${newNumber} is already added to the phonebook`);
+      setNewNumber('');
+    } else {
+      setPersons(persons.concat({name: newName, number: newNumber}));
+      setNewName('');
+      setNewNumber('');
+    }    
   }
 
   const handleNameChange = (event) => {
     // console.log(event.target.value);
     setNewName(event.target.value);
+  }
+
+  const handleNumberChange = (event) => {
+    // console.log(event.target.value);
+    setNewNumber(event.target.value); 
   }
 
   return (
@@ -44,6 +49,14 @@ const App = () => {
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          number: <input 
+                    value={newNumber} 
+                    onChange={handleNumberChange}
+                    type="tel" 
+                    pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" // 123-456-7890
+                  />
         </div>
         <div>
           <button type="submit" >add</button>
